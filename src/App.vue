923c147b95d2110f1400952e63010424c35aeff3
nhuquynhnguyenle <56589737+nhuquynhnguyenle@users.noreply.github.com>
2025-11-01 18:09:13 +0700

    Lesson 3 Start

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <script setup>
 import { ref } from 'vue'
-
+import socksGreenImage from './assets/images/socks_green.jpeg'
 const product = ref('Socks')
-
+const image = ref(socksGreenImage)
+const inventory = ref(-2)
 </script>
 
 <template>
+  <div class="nav-bar"></div>
   <div class="product-display">
     <div class="product-container">
       <div class="product-image">
-        <!-- image goes here -->
+        <img :src="image">
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
+        <p
+          v-if ="inventory > 10">In Stock
+        </p>
+        <p
+          v-else-if="inventory <= 10 && inventory > 0" > Almost sold out!
+        </p>
+        <p
+          v-else> Out of stock
+        </p>
       </div>
     </div>
   </div>
